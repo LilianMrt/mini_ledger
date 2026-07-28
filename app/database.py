01e361bb74cb396aab78a99ba3edff6b817cf801
Lilian_Mrt <lilian.maroteau@gmail.com>
2026-07-28 10:20:00 +0200

    Add asyncpg connection pool

---
+++ app/database.py
@@ -0,0 +1,24 @@
+import os
+import asyncpg
+
+_pool = None
+
+async def init_db_pool():
+    """Initializes a global connection pool."""
+    global _pool
+    if not _pool:
+        _pool = await asyncpg.create_pool(
+            host= os.getenv("DB_HOST"),
+            port=5432,
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            min_size=5,
+            max_size=20
+        )
+
+async def get_db():
+    """Yields a database connection from the pool."""
+    global _pool
+    async with _pool.acquire() as connection:
+        yield connection
